c314274ae50ab5a6b721a0a7402a51e7ef22d3df
Fwang0222 <1441777902@qq.com>
2024-12-07 17:53:27 +0800

    Code created from Part 2
    I updated the code I created earlier as required in Part 2, abstracted the People and Ride classes from being instantiated, created an interface for the Ride class, and defined eight methods for it, which I tested in my main function.

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -1,5 +1,30 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
+        Employee operator1 = new Employee("Jack", 25, "Male", "1234567", 101, "Operation department", "None");
+        Visitor visitor1 = new Visitor("Amy", 19, "Female", "1234568","175cm", "123@ab.com", true);
+        Visitor visitor2 = new Visitor("James", 28, "Male", "12345679", "179cm", "124@ab.com", false);
+
+        class RollerCoaster extends Ride {
+            public RollerCoaster(String rideName, int rideId, Employee rideOperator, String suitablePopulation) {
+                super(rideName, rideId, rideOperator, suitablePopulation);
+            }
+        }
+
+        RollerCoaster rollerCoaster = new RollerCoaster("Roller Coaster", 1, operator1, "up to 170cm");
+
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+
+        rollerCoaster.printQueue();
+
+        rollerCoaster.runOneCycle();
+
+        rollerCoaster.addVisitorToHistory(visitor1);
+        rollerCoaster.addVisitorToHistory(visitor2);
+
+        rollerCoaster.printRideHistory();
+
+        System.out.println("Is Amy in history? " + rollerCoaster.checkVisitorFromHistory(visitor1));
 
     }
 
--- Ride.java
+++ Ride.java
@@ -1,14 +1,33 @@
-public class Ride {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+interface RideInterface {
+    void addVisitorToQueue(Visitor visitor);
+    void removeVisitorFromQueue(Visitor visitor);
+    void printQueue();
+    void runOneCycle();
+    void addVisitorToHistory(Visitor visitor);
+    boolean checkVisitorFromHistory(Visitor visitor);
+    int numberOfVisitors();
+    void printRideHistory();
+}
+
+abstract class Ride implements RideInterface {
     private String rideName;
     private int rideId;
     private Employee rideOperator;
     private String suitablePopulation;
+    private List<Visitor> queue;
+    private List<Visitor> rideHistory;
 
     public Ride() {
         this.rideName = "Unknown";
         this.rideId = 0;
         this.rideOperator = null;
         this.suitablePopulation = "Unknown";
+        this.queue = new LinkedList<>();
+        this.rideHistory = new LinkedList<>();
     }
 
     public Ride(String rideName, int rideId, Employee rideOperator, String suitablePopulation) {
@@ -16,6 +35,54 @@ public class Ride {
         this.rideId = rideId;
         this.rideOperator = rideOperator;
         this.suitablePopulation = suitablePopulation;
+        this.queue = new LinkedList<>();
+        this.rideHistory = new LinkedList<>();
+    }
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        queue.add(visitor);
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        queue.remove(visitor);
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Visitors in the queue for " + rideName + ":");
+        for (Visitor v : queue) {
+            System.out.println(v.getName());
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        System.out.println(rideName + " is running one cycle.");
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        rideHistory.add(visitor);
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return rideHistory.contains(visitor);
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return rideHistory.size();
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("Ride history for " + rideName + ":");
+        for (Visitor v : rideHistory) {
+            System.out.println(v.getName());
+        }
     }
 
     public String getRideName() {
